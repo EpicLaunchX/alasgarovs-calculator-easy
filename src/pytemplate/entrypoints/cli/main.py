211e9aaedf660a3ab0a2e5a5ca5028fad65486b9
sabuhi <test@gmail.com>
2024-05-03 16:04:24 +0400

    feat: Refactor main() function

--- src/pytemplate/entrypoints/cli/main.py
+++ src/pytemplate/entrypoints/cli/main.py
@@ -1,35 +1,28 @@
-from src.pytemplate.domain.models import operands_factory
-from src.pytemplate.service.calculator import Calculator
-
-
-def get_operands():
-    first_operand = int(input("Enter the first operand: "))
-    second_operand = int(input("Enter the second operand: "))
-    return operands_factory(first_operand, second_operand)
-
-
-def perform_calculation(operands, action):
-    calculator = Calculator()  # Create an instance of Calculator
-    if action == "add":
-        return calculator.add(operands)
-    elif action == "subtract":
-        return calculator.subtract(operands)
-    elif action == "multiply":
-        return calculator.multiply(operands)
-    elif action == "divide":
-        return calculator.divide(operands)
-    else:
-        raise ValueError("Invalid action. Please enter add/subtract/multiply/divide.")
+from pytemplate.domain.models import operands_factory
+from pytemplate.service.calculator import Calculator
 
 
 def main():
     print("Calculator CLI!")
 
-    operands = get_operands()
+    first_operand = int(input("Enter the first operand: "))
+    second_operand = int(input("Enter the second operand: "))
     action = input("Enter the action (add/subtract/multiply/divide): ")
 
-    try:
-        result = perform_calculation(operands, action)
-        print("Result:", result)
-    except ValueError as e:
-        print("Error:", e)
+    # Create Calculator instance and operands object
+    calculator = Calculator()
+    operands = operands_factory(first_operand, second_operand)
+
+    if action == "add":
+        result = calculator.add(operands)
+    elif action == "subtract":
+        result = calculator.subtract(operands)
+    elif action == "multiply":
+        result = calculator.multiply(operands)
+    elif action == "divide":
+        result = calculator.divide(operands)
+    else:
+        print("Invalid action. Please enter add/subtract/multiply/divide.")
+        return
+
+    print("Result:", result)
